4a90f3a35abf8f13cb6d791a3f59f8d54c0a4f5c
Tom Ridge <thomas.ridge@cultureamp.com>
2025-01-31 19:58:34 +1030

    fix bindings

--- init.lua
+++ init.lua
@@ -90,8 +90,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.api.nvim_set_keymap('n', '^S', '<cmd>:w<cr>', { noremap = true })
-vim.api.nvim_set_keymap('i', '^S', '<cmd>:w<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-s>', '<cmd>:w<cr>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-s>', '<cmd>:w<cr>', { noremap = true })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
